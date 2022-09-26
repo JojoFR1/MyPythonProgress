c8ddefcb154e761fd653a866cb845134c287f88d
JojoFR1 <jojocraft692@gmail.com>
2022-09-26 19:09:47 +0200

    Prototype V4 (fonctionne)

--- Hangman/Pendu.py
+++ Hangman/Pendu.py
@@ -6,8 +6,6 @@ import random
 import json
 import time
 
-# import time
-
 ##Settings
 def settings():
     global word
@@ -15,7 +13,8 @@ def settings():
     global guessed
     global fail
     global err
-    global hangman
+    global limit
+    global replay
     
     with open("Hangman/wordList.json", "r") as read_file:
         wordList = json.load(read_file)
@@ -27,6 +26,25 @@ def settings():
     err = 0
     limit = 8
     
+    replay = ""
+
+def playAgain():
+    global replay
+    replay = input("\nDo you want to play again? Y/N \n")
+    while replay.lower() not in ["y", "n"]:
+        playAgain()
+    if replay.lower() == "y":
+        settings()
+    elif replay.lower() == "n":
+        exit()
+        
+    
+def game():
+    global word
+    global display
+    global guessed
+    global fail
+    global err
     hangman = [f'''
                         
                 
@@ -35,9 +53,9 @@ def settings():
                 
                 
                 
-                
+                You have {limit - err} guesses.
                 Input a letter to guess: ''', f'''
-                        
+            
                 
                             Word: {display}
                             You tried: {fail}
@@ -56,7 +74,7 @@ def settings():
                 Wrong guess: {limit - err} guesses remaining.
                 Input a letter to guess: ''', f'''
                 +----+       
-                |    |
+                |
                 |           Word: {display}
                 |           You tried: {fail}
                 |
@@ -110,60 +128,39 @@ def settings():
                 Wrong guess!
                 You lost, the word was {word}!''']
 
-def game():
-    global word
-    global display
-    global guessed
-    global fail
-    global err
-
-    if err == 0:
-        guess = input(hangman[0])
-    elif err == 1:
-        guess = input(hangman[1])
-    elif err == 2:
-        guess = input(hangman[2])
-    elif err == 3:
-        guess = input(hangman[3])
-    elif err == 4:
-        guess = input(hangman[4])
-    elif err == 5:
-        guess = input(hangman[5])
-    elif err == 6:
-        guess = input(hangman[6])
-    elif err == 7:
-        guess = input(hangman[7])
-    elif err == 8:
+    if err == 8:
         print(hangman[8])
-        exit() ##TODO: Replay system
-
+        playAgain()
+        
+    guess = input(hangman[err])
     guess = guess.strip()
-    guess = guess[0]
-    ##Something is breaking here
+
     if len(guess.strip()) == 0 or len(guess.strip()) >= 2: #Check if the input is valid
         print("Invalid input!")
-        time.sleep(2.5)
+        time.sleep(1)
         game()
     elif guess in guessed: #Chech if the input was already guessed
         print(f"You already guessed: {guess}!")
-        time.sleep(2.5)
+        time.sleep(1)
         game()
-    elif guess in fail:
+    elif guess in fail: #Check if the input was already tried
         print(f"You already tried: {guess}!")
-        time.sleep(2.5)
+        time.sleep(1)
         game()
     elif guess in word:
-        guessed.extend([guess])
+        guessed.append(guess)
         index = word.find(guess)
         display = display[:index] + guess + display[index +1:]
+        print(display)
         game()
     else:
         err += 1
-        fail = fail.append([guess])
+        fail.append(guess)
         game()
 
     if display == word:
         print("You found the secret word, you won!")
+        playAgain()
 
 settings()
 game()
