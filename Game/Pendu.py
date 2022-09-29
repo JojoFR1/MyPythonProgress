e0a848be0e111f5c3af25993fa46224205dfe82b
JojoFR1 <jojocraft692@gmail.com>
2022-09-29 10:48:49 +0200

    Pendu version final?

--- Game/Pendu.py
+++ Game/Pendu.py
@@ -1,5 +1,5 @@
 ## Programme du 22/09/2022
-## Dernière modification le 26/09/2022
+## Dernière modification le 29/09/2022
 
 # Pendu
 import random
@@ -8,16 +8,19 @@ import time
 
 ##Settings
 def settings(): #Set-up everything for the game
+    global alphabet
     global word
     global display
     global guessed
     global fail
     global err
-    global limit   
-    with open("Hangman/wordList.json", "r") as read_file: #TODO: Make a real list, in french
+    global limit
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    with open("Game/wordList.json", "r") as read_file: #TODO: Make a real list
         wordList = json.load(read_file)
     
     word = random.choice(wordList)
+    word = word.upper()
     display = "_" * len(word)
     guessed = []
     fail = []
@@ -25,120 +28,123 @@ def settings(): #Set-up everything for the game
     limit = 8
 
 def playAgain(): #Ask the user if he want to play again when he lose/win
-    replay = input("\nDo you want to play again? Y/N \n")
-    if replay.lower() == "y":
+    replay = input("\nDo you want to play again? (Y/N) ")
+    if replay.upper() == "Y":
         replay = ""
         settings()
-    elif replay.lower() == "n":
+    elif replay.upper() == "N":
         exit()
     else:
         playAgain()
-        
-    
+
 def game():
     global word
     global display
     global guessed
     global fail
     global err
-    
+
     #All 9 stages of the hangman drawing
     hangman = [f'''
-                        
-                
+
+
                             Word: {display}
                             You tried: {fail}
-                
-                
-                
+
+
+
                 You have {limit - err} guesses.
                 Input a letter to guess: ''', f'''
-            
-                
+
+
                             Word: {display}
                             You tried: {fail}
-                
+
                =========
-                
+
                 Wrong guess: {limit - err} guesses remaining.
                 Input a letter to guess: ''', f'''
-                            
+
                 |
                 |           Word: {display}
                 |           You tried: {fail}
                 |
                =========
-                
+
                 Wrong guess: {limit - err} guesses remaining.
                 Input a letter to guess: ''', f'''
-                +----+       
+                +----+
                 |
                 |           Word: {display}
                 |           You tried: {fail}
                 |
                ========
-                
+
                 Wrong guess: {limit - err} guesses remaining.
                 Input a letter to guess: ''', f'''
-                +----+      
+                +----+
                 |/   |
                 |           Word: {display}
                 |           You tried: {fail}
                 |
                =========
-                
+
                 Wrong guess: {limit - err} guesses remaining.
                 Input a letter to guess: ''', f'''
-                +----+      
+                +----+
                 |/   |
                 |    O      Word: {display}
                 |           You tried: {fail}
                 |
                =========
-                
+
                 Wrong guess: {limit - err} guesses remaining.
                 Input a letter to guess: ''', f'''
-                +----+      
+                +----+
                 |/   |
                 |    O      Word: {display}
                 |    |      You tried: {fail}
                 |
                =========
-                
+
                 Wrong guess: {limit - err} guesses remaining.
                 Input a letter to guess: ''', f'''
-                +----+      
+                +----+
                 |/   |
                 |    O      Word: {display}
                 |   /|\\    You tried: {fail}
                 |
                =========
-                
+
                 Wrong guess: {limit - err} guesses remaining.
                 Input a letter to guess: ''', f'''
-                +----+      
+                +----+
                 |/   |
                 |    O      Word: {display}
                 |   /|\\    You tried: {fail}
                 |   / \\
                =========
-                
+
                 Wrong guess!
                 You lost, the word was {word}!''']
 
+    if display == word: #Check if the user won the game (found the word)
+        print("You found the secret word, you won!")
+        time.sleep(0.5)
+        playAgain()
     if err == 8: #Check if the user lost the game (max 8 fails)
         print(hangman[8])
         time.sleep(0.5)
         playAgain()
-        
+
     guess = input(hangman[err]) #Get user input to guess a letter
-    if guess == "stop": #Dev Only!
+    if guess == "stop": #To stop the game (Dev Only!)
         exit()
-    
-    guess = guess.lower()
+
+    guess = guess.upper()
     guess = guess.strip()
 
-    if len(guess.strip()) == 0 or len(guess.strip()) >= 2: #Check if the input is valid
+    if guess not in alphabet or len(guess.strip()) == 0 or len(guess.strip()) >= 2: #Check if the input is a letter and is valid
         print("Invalid input!")
         time.sleep(1)
         game()
@@ -159,11 +165,6 @@ def game():
         err += 1
         fail.append(guess)
         game()
-
-    if display == word: #TODO: Win not working
-        print("You found the secret word, you won!")
-        time.sleep(0.5)
-        playAgain()
 
 settings()
 game()
--- README.md
+++ README.md
@@ -1,11 +1,17 @@
+# My Python Progress
+
 This is a personnal repo just to show my progress with **Python**!
 
 - - -
+- - -
 
-## [Presentation.py](https://github.com/JojoFR1/MyPythonProgress/blob/master/Presentation.py)
+## [Presentation.py](https://github.com/JojoFR1/MyPythonProgress/blob/master/Other/Presentation.py) - 15/09/2022
 
 This program was made for a simple homework, and this is my first one :)
 
 - - -
+- - -
 
-## [Pendu.py](https://github.com/JojoFR1/MyPythonProgress/blob/master/Hangman/Pendu.py)
+## [Pendu.py](https://github.com/JojoFR1/MyPythonProgress/blob/master/Game/Pendu.py) - 22/09/2022
+
+Second program, and I made a hangman game!
