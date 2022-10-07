6e4bf2c5b8b3bd41f506745885a8d98e859f09ba
JojoFR1 <jojocraft692@gmail.com>
2022-10-08 01:13:52 +0200

    TicTacToe V3 (almost working)

--- Game/TicTacToe.py
+++ Game/TicTacToe.py
@@ -1,31 +1,47 @@
 ## Programme du 01/10/2022
-## Dernière modification le 05/10/2022
+## Dernière modification le 08/10/2022
 
 # Tic Tac Toe
 import time
-scoreP1 = 0 #Set player 1 score to 0 when the program start
-scoreP2 = 0 #Set player 2 score to 0 when the program start
+num = "123456789" #Var to check if the input is correct
+score = 0, 0 #Set player 1 and player 2 score to 0 when the program start
+player = 1 #Set player to 1 only when the program start
+
+allowedSymbol = "O X # & $ € %"
+def symbolSelect():
+    global symbolP1, symbolP2
+    symbolP1 = input(f"Player 1, please select your symbol ({allowedSymbol}): ").upper()
+    if symbolP1 not in allowedSymbol or len(symbolP1) != 1:
+        print("Invalid input")
+        symbolSelect()
+    symbolP2 = input(f"Player 2, please select your symbol ({allowedSymbol}): ").upper()
+    if symbolP2 not in allowedSymbol or len(symbolP2) != 1: #Check if the input is a allowed symbol and is valid
+        print("Invalid input")
+        symbolSelect()
+    if symbolP1 or symbolP2 == 's': #Stop the program (Dev only!)
+        exit()
+symbolSelect()
+
 
 def settings():
-    global boardLayout, num, filled, player, scoreP1, scoreP2, symbol
+    global boardLayout, filled, win, move, score, solutions, symbol
     boardLayout = "123456789"
-    num = "123456789"
     filled = []
-    player = 1
-    scoreP1 = scoreP1 #Keep the score between game (only when replayed)
-    scoreP2 = scoreP2 #Keep the score between game (only when replayed)
-    
-    allowedSymbol = "OX"
-    symbolSelect = input("Player 1, please select your symbol (O or X): ").upper()
-    if symbolSelect not in allowedSymbol or len(symbolSelect) != 1: #Check if the input is a allowed symbol and is valid
-        print("Invalid input")
-        settings()
-    elif symbolSelect == "O": #Check the player 1 input to set the correct order
-        symbol = [symbolSelect, "X"]
-        game() #BUG: When the game is replayed, it just stop the game, to fix it, this line is required
-    else:
-        symbol = [symbolSelect, "O"]
-        game() #BUG: When the game is replayed ,it just stop the game, to fix it, this line is required
+    win = False
+    move = 9
+    score = score #Keep the score between game (only when replayed)
+    solutions = [#Horizontal
+                 [0,1,2],
+                 [3,4,5],
+                 [6,7,8],
+                 #Vertical
+                 [0,3,6],
+                 [1,4,7],
+                 [2,5,8],
+                 #Diagonal
+                 [0,4,8],
+                 [2,4,6]]
+    symbol = [symbolP1, symbolP2]
 
 def playAgain(): #Ask the user if he want to play again when he lose/win
     replay = input("\nDo you want to play again? (Y/N) ").upper()
@@ -33,11 +49,23 @@ def playAgain(): #Ask the user if he want to play again when he lose/win
         settings()
     elif replay == "N": #If the input is = "N", then the program stop
         exit()
-    else:
+    else: #If the input is invalid, ask again
         playAgain()
 
 def game():
-    global player, boardLayout, scoreP1, scoreP2
+    global boardLayout, player, score, win, move
+    
+    for solution in solutions:
+        line = boardLayout[solution[0]] == boardLayout[solution[1]] == boardLayout[solution[2]]
+        if line:
+            if line == symbol[0]: #If the symbol is equal to player 1 symbol, then
+                player = 1 #Set player to Player 1
+                score = score[0] + 1, score[1] #Add 1 to Player 1 score, keep Player 2 score the same
+                win = True
+            elif line == symbol[1]: #If the symbol is equal to player 2 symbol, then
+                player = 2 #Set the player to Player 2
+                score = score[0], score[1] + 1 #Keep Player 1 score the same, add 1 to Player 2 score
+                win = True
     
     #The Tic-Tac-Toe board with multiple variants, when the game is played, when the game is finished and when the game is a tie
     board = [f'''
@@ -45,50 +73,58 @@ def game():
               
                                  {boardLayout[0]} | {boardLayout[1]} | {boardLayout[2]}
                                 ===+===+===       SCORE
-                                 {boardLayout[3]} | {boardLayout[4]} | {boardLayout[5]}     Player 1: {scoreP1}
-                                ===+===+===    Player 2: {scoreP2}
+                                 {boardLayout[3]} | {boardLayout[4]} | {boardLayout[5]}     Player 1: {score[0]}
+                                ===+===+===    Player 2: {score[1]}
                                  {boardLayout[6]} | {boardLayout[7]} | {boardLayout[8]}
                 
                       Select where to place your symbol: ''', f'''
-                             PLAYER {player} ({symbol[player-1]}) TURN
+                              PLAYER {player} ({symbol[player-1]}) WON
               
                                  {boardLayout[0]} | {boardLayout[1]} | {boardLayout[2]}
                                 ===+===+===       SCORE
-                                 {boardLayout[3]} | {boardLayout[4]} | {boardLayout[5]}     Player 1: {scoreP1}
-                                ===+===+===    Player 2: {scoreP2}
+                                 {boardLayout[3]} | {boardLayout[4]} | {boardLayout[5]}     Player 1: {score[0]}
+                                ===+===+===    Player 2: {score[1]}
                                  {boardLayout[6]} | {boardLayout[7]} | {boardLayout[8]}
                 
                       Congrats! Player {player} ({symbol[player-1]}) won the game!''', f'''
-                             PLAYER {player} ({symbol[player-1]}) TURN
+                                 IT\'S A TIE
               
                                  {boardLayout[0]} | {boardLayout[1]} | {boardLayout[2]}
                                 ===+===+===       SCORE
-                                 {boardLayout[3]} | {boardLayout[4]} | {boardLayout[5]}     Player 1: {scoreP1}
-                                ===+===+===    Player 2: {scoreP2}
+                                 {boardLayout[3]} | {boardLayout[4]} | {boardLayout[5]}     Player 1: {score[0]}
+                                ===+===+===    Player 2: {score[1]}
                                  {boardLayout[6]} | {boardLayout[7]} | {boardLayout[8]}
                 
                       It's a tie! No one won, better luck next time!''']
-                
-    place = input(board[0])
-    place.strip()
-    if place == "s": #To stop the game (Dev Only!)
+    if win: #If one of the player won, then ask the player if he wants to play again
+        print(board[1]) #Show the winning board
+        playAgain()
+    if move == 0: #If move is equal to zero, it means the game is a tie
+        print(board[2])
+        playAgain()
+    
+    case = input(board[0])
+    print(move)
+    case.strip()
+    if case == "s": #To stop the game (Dev Only!)
         exit()
     
-    if place not in num or len(place) !=1: #Check if the input is a valid number and is valid
+    if case not in num or len(case) !=1: #Check if the input is a valid number and is valid
         print("Invalid input!")
         time.sleep(1)
         game()
-    elif place in filled: #Check if the case is already filled
+    elif case in filled: #Check if the case is already filled
         print("This case already have a symbol!")
         time.sleep(1)
         game()
     else:
-        boardLayout = boardLayout.replace(place, symbol[player-1]) #Replace the case input with the player symbol
+        boardLayout = boardLayout.replace(case, symbol[player-1]) #Replace the case input with the player symbol
         if player == 1: #If it was player 1 turn, then add 1 to player to become player 2.
             player += 1
         else: #If it was not player 1 turn, then remove 1 to player to become player 1.
             player -= 1
-        filled.append(place)
+        filled.append(case) #Add the input case to the filled list
+        move -= 1
         game()
 
 settings()
